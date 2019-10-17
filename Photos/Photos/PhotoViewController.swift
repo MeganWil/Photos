cb7f9fbb462b21279c62878b18086080c660a405
Megan Wilson <mlw7v3@mail.missouri.edu>
2019-10-17 00:07:36 -0500

    Finished code and tested on iphone

--- Photos/Photos/PhotoViewController.swift
+++ Photos/Photos/PhotoViewController.swift
@@ -8,23 +8,61 @@
 
 import UIKit
 
-class PhotoViewController: UIViewController {
+class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var selectedImage: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        imagePicker.delegate = self
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func cameraSelected(_ sender: UIBarButtonItem){
+        photoTaken()
     }
-    */
-
+    
+    @IBAction func librarySelected(_ sender: Any) {
+        selectFromLibrary()
+    }
+    
+    func photoTaken(){
+        if(!UIImagePickerController.isSourceTypeAvailable(.camera)) {
+            alert(title: "No Camera", message: "This device has no camera.")
+        }
+        else{
+            imagePicker.allowsEditing = false
+            imagePicker.sourceType = .camera
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+   
+    func selectFromLibrary() {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+   
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                   selectedImage.image = pickedImage
+               }
+               dismiss(animated: true, completion: nil)
+    }
+    
+    func alert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+               alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+               present(alert, animated: true, completion: nil)
+    }
+    
+    func imageDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
